56e5ad0b0b6d65adb432a1cf7d570897e3925432
Thomas Antony <tantony@purdue.edu>
2018-04-27 19:18:15 -0500

    Removed extra stuff.

--- src/blinky.cpp
+++ src/blinky.cpp
@@ -1,18 +1,11 @@
 #include "application.h"
-//
+
 SYSTEM_MODE(AUTOMATIC);
-//
+
 int LED = D0;                         // LED is connected to D1
-#pragma message("ASSSS!")
 
 void setup() {
-    Serial.begin(115200);
     pinMode(LED, OUTPUT);               // sets pin as output
-    // pinMode(D1, OUTPUT);          // sets the LED on
-    // digitalWrite(D1, HIGH);          // sets the LED on
-    // WiFi.connect();
-    // waitUntil(WiFi.ready);
-    // digitalWrite(D1, LOW);          // sets the LED on
 }
 
 void loop() {
@@ -20,9 +13,7 @@ void loop() {
     Particle.connect();
   }
   digitalWrite(LED, HIGH);          // sets the LED on
-  delay(2000);                       // waits for 200mS
+  delay(1000);                       // waits for 200mS
   digitalWrite(LED, LOW);           // sets the LED off
   delay(1000);                       // waits for 200mS
-  Serial.println("ASSS!!!");
-  // delay(1000);
 }
